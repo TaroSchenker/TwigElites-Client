5e6e1428404b6eaec18d10b51a0e6846463b9a29
Florencia Pezcara <fpezcara@gmail.com>
2022-08-26 17:31:14 +0100

    added fetch requests for /signin and /signup

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,20 +1,62 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 import { Container, Col, Row } from "react-bootstrap";
 
 const Signin = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [messageForUser, setMessageForUser] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const options = {
+      username: username,
+      password: password,
+    };
+    try {
+      const { data } = await axios.post(
+        "https://test-twiglets.herokuapp.com/auth/login",
+        options
+      );
+      // saving token in sessionStorage - when user logs out this token needs to be removed
+      sessionStorage.setItem("token", data.access_token);
+      setMessageForUser(data);
+    } catch (err) {
+      console.error("Oops, there's been an error: ", err);
+    }
+  };
+  console.log(sessionStorage.getItem("token"));
+
   return (
     <div className="packet">
-        <form className="sign-up overlay">
-            <h2 className="caveat fm-header">Fellow TwigElite</h2>
-            <br/>
-            <input className="caveat" type="text" placeholder="NAME"/>
-            <br/>
-            <input className="caveat" type="password" placeholder="PASSWORD 1"/>
-            <br/>
-            <input className="special-btn caveat" type="submit" value="LOGIN"/>
-        </form>
+      <form className="sign-up overlay">
+        <h2 className="caveat fm-header">Fellow TwigElite</h2>
+        <br />
+        <input
+          className="caveat"
+          type="text"
+          placeholder="NAME"
+          onChange={(e) => setUsername(e.target.value)}
+          value={username}
+        />
+        <br />
+        <input
+          className="caveat"
+          type="password"
+          placeholder="PASSWORD 1"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+        <br />
+        <input
+          className="special-btn caveat"
+          type="submit"
+          value="LOGIN"
+          onClick={(e) => handleLogin(e)}
+        />
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
--- src/pages/Registry/index.js
+++ src/pages/Registry/index.js
@@ -6,28 +6,28 @@ import Signin from "../../components/Signin";
 import { useState } from "react";
 
 const Registry = () => {
-    const [isShownLog, setIsShownLog] = useState(false);
+  const [isShownLog, setIsShownLog] = useState(true);
 
-    const handleClick = (e) => {
-        e.preventDefault()
-        setIsShownLog(current => !current);
-      };
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsShownLog((current) => !current);
+  };
 
-    return (
-        <Container>
-        <Row>
-            <Col s={12} className=" bg-warning">
-            nav bar here
-            </Col>
-        </Row>
-        <Row className="bg-warning mt-4" style={{ height: "80vh" }}>
-            <Col className="bg-primary d-flex justify-content-center align-items-center">
-                { isShownLog ? <Signin /> : <Signup /> }
-                <button onClick={e => handleClick(e)}></button>
-            </Col>
-        </Row>
-        </Container>
-    );
+  return (
+    <Container>
+      <Row>
+        <Col s={12} className=" bg-warning">
+          nav bar here
+        </Col>
+      </Row>
+      <Row className="bg-warning mt-4" style={{ height: "80vh" }}>
+        <Col className="bg-primary d-flex justify-content-center align-items-center">
+          {isShownLog ? <Signin /> : <Signup />}
+          <button onClick={(e) => handleClick(e)}></button>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Registry;
