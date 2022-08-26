38bc6097764895e1581b228089d5aa3537d180eb
“iamthechanger” <taroschenker@hotmail.co.uk>
2022-08-26 11:06:01 +0100

    layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
-import {Container, Col, Row } from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+
+import { Routes, Route } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Home } from "./pages";
+import Layout from "./layout";
 
 function App() {
   return (
-    <Container  >
-    <Row>
-    <Col s={12} className=" bg-warning">1 of 3</Col>
-    </Row>
-      <Row className="mt-4" style={{height: '80vh'}}>
-        <Col s={12} md={3} className="mx-2 bg-success">1 of 3</Col>
-        <Col s={12} md={5} className="mx-2 bg-info">Map</Col>
-        <Col s={12} md={3} className="mx-2 bg-danger">3 of 3</Col>
-      </Row>
-
-    </Container>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Home />}>
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
+import Sidebar from "../../components/Sidebar";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Container>
+      <Row>
+        <Col s={12} className=" bg-warning">
+        nav bar here
+        </Col>
+      </Row>
+      <Row className="mt-4" style={{ height: "80vh" }}>
+        <Col s={12} md={3} className="mx-2 bg-primary">
+        <Sidebar />
+        </Col>
+        <Col s={12} md={5} className="mx-2 bg-info">
+          Map
+        </Col>
+        <Col s={12} md={3} className="mx-2 bg-danger">
+        <Sidebar />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
-export default Home
+export default Home;
