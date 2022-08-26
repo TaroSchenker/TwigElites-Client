138abb56ae52c461c78db1b975d573cc238eeef0
“iamthechanger” <taroschenker@hotmail.co.uk>
2022-08-26 21:52:43 +0100

    map working with search

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,39 +1,201 @@
-import React, {useState} from "react";
-import GoogleMapReact from "google-map-react";
-import LocationMarker from "../LocationMarker";
-import Locationinfobox from "../LocationInfo";
+import React from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
+import { formatRelative } from "date-fns";
 
-;
+import "@reach/combobox/styles.css";
+import mapStyles from "./mapStyles";
 
-const Map = ({ eventData, center, zoom }) => {
-    const [locationInfo, setLocationInfo] = useState(null)
-    const markers = eventData.map(e => {
-        if(e.categories[0].id === 8){
-            return  <LocationMarker lat={e.geometries[0].coordinates[1]} lng={e.geometries[0].coordinates[0]} onClick={() => setLocationInfo({id: e.id, title: e.title})}/>
-        }
-        return null
-    })
+const libraries = ["places"];
+const mapContainerStyle = {
+  position: "relative",
+  height: "40em",
+  width: "100%",
+};
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+const center = {
+  lat: 51.5072,
+  lng: -0.1276,
+};
 
-  return ( 
-    <div className="map">
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyCd4UUo7DDCuMoSgBQ_PFgcH0OOFxcCdT4" }}
-        defaultCenter={center}
-        defaultZoom={zoom}
+export default function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+
+  const onMapClick = React.useCallback((e) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  const panTo = React.useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
+
+  if (loadError) return "Error";
+  if (!isLoaded) return "Loading...";
+
+  return (
+    <div>
+      <h1 className="map-title">
+        Bears
+      
+      </h1>
+
+      <Locate panTo={panTo} />
+      <Search panTo={panTo} />
+
+      <GoogleMap
+        id="map"
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
       >
-        {markers}
-      </GoogleMapReact>
-      {locationInfo && <Locationinfobox info={locationInfo} /> }
+        {markers.map((marker) => (
+          <Marker
+            key={`${marker.lat}-${marker.lng}`}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+            icon={{
+              url: `/twiglets-logo-png-transparent.png`,
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(45, 45),
+              scaledSize: new window.google.maps.Size(90, 90),
+            }}
+          />
+        ))}
+
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>
+                Alert
+              </h2>
+              <p>Found Originals! {formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
     </div>
   );
-};
+}
 
-Map.defaultProps = {
-  center: {
-    lat: 42.3265,
-    lng: -122.8756,
-  },
-  zoom: 6,
-};
+function Locate({ panTo }) {
+  return (
+    <button
+      className="locate"
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            panTo({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            });
+          },
+          () => null
+        );
+      }}
+    >Find my location
+      {/* <img src="/compass.svg" alt="compass" /> */}
+    </button>
+  );
+}
 
-export default Map;
+function Search({ panTo }) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 51.5072, lng: () => -0.1276 },
+      radius: 100 * 1000,  
+    },
+  });
+
+  // https://developers.google.com/maps/documentation/javascript/reference/places-autocomplete-service#AutocompletionRequest
+
+  const handleInput = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSelect = async (address) => {
+    setValue(address, false);
+    clearSuggestions();
+
+    try {
+      const results = await getGeocode({ address });
+      const { lat, lng } = await getLatLng(results[0]);
+      panTo({ lat, lng });
+    } catch (error) {
+      console.log("😱 Error: ", error);
+    }
+  };
+
+  return (
+    <div className="search">
+      <Combobox onSelect={handleSelect}>
+        <ComboboxInput
+          value={value}
+          onChange={handleInput}
+          disabled={!ready}
+          placeholder="Search your location"
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === "OK" &&
+              data.map(({ id, description }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
+    </div>
+  );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,22 +4,22 @@ import { Map } from "../../components";
 import Sidebar from "../../components/Sidebar";
 
 const Home = () => {
-    const [eventData, setEventData] = useState([])
+    // const [eventData, setEventData] = useState([])
     const [loading, setLoading] = useState(false)
   
-    useEffect(() => {
-      const fetchEvents = async () => {
-        console.log('i fetch events')
-          setLoading(true)
-          const res = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events')
-          const { events } = await res.json()
-          setEventData(events)
-          setLoading(false)    
-      }
-      fetchEvents()
-      console.log(eventData) 
+    // useEffect(() => {
+    //   const fetchEvents = async () => {
+    //     console.log('i fetch events')
+    //       setLoading(true)
+    //       const res = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events')
+    //       const { events } = await res.json()
+    //       setEventData(events)
+    //       setLoading(false)    
+    //   }
+    //   fetchEvents()
+    //   console.log(eventData) 
   
-    },[])
+    // },[])
 
   return (
     <Container>
@@ -28,14 +28,14 @@ const Home = () => {
         nav bar here
         </Col>
       </Row>
-      <Row className="mt-4" style={{ height: "80vh" }}>
-        <Col s={12} md={3} className="mx-2 bg-primary">
+      <Row className="" style={{ height: "90vh" }}>
+        <Col s={12} md={3} className="bg-primary">
         <Sidebar />
         </Col>
-        <Col s={12} md={5} className="mx-2 bg-transparent">
-        {!loading  ? <Map eventData={eventData} /> : <h1 className='loader'> loading</h1>}
+        <Col s={12} md={6} className=" bg-transparent">
+        {!loading  ? <Map  /> : <h1 className='loader'> loading</h1>}
         </Col>
-        <Col s={12} md={3} className="mx-2 bg-danger">
+        <Col s={12} md={3} className=" bg-danger">
         <Sidebar />
         </Col>
       </Row>
