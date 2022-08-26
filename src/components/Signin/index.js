3a9c45b7aa1f3bfd39df8fdad03ee8ccce759dc4
reececilon <reecethat@gmail.com>
2022-08-26 17:15:16 +0100

    Signup/in finished

--- src/components/Signin/index.js
+++ src/components/Signin/index.js
@@ -1,23 +1,20 @@
 import React from 'react'
 import { Container, Col, Row } from "react-bootstrap";
 
-const Signup = () => {
+const Signin = () => {
   return (
     <div className="packet">
         <form className="sign-up overlay">
-            <h2 className="caveat fm-header">Become a TwigElite</h2>
-            <input className="caveat" type="text" placeholder="EMAIL"/>
+            <h2 className="caveat fm-header">Fellow TwigElite</h2>
             <br/>
             <input className="caveat" type="text" placeholder="NAME"/>
             <br/>
             <input className="caveat" type="password" placeholder="PASSWORD 1"/>
             <br/>
-            <input className="caveat" type="password" placeholder="PASSWORD 2"/>
-            <br/>
-            <input className="special-btn caveat" type="submit" value="JOIN"/>
+            <input className="special-btn caveat" type="submit" value="LOGIN"/>
         </form>
     </div>
   )
 }
 
-export default Signup
+export default Signin
