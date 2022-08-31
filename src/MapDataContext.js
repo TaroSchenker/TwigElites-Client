048580832cb84dd0152540fe2d80cf7ee4a41b43
“iamthechanger” <taroschenker@hotmail.co.uk>
2022-08-31 20:54:30 +0100

    fix vote

--- src/MapDataContext.js
+++ src/MapDataContext.js
@@ -11,6 +11,7 @@ export default function MapDataProvider(props) {
   const [gotoTwiglet, setGotoTwiglet] = useState(null);
   const [initialLocation, setInitialLocation] = useState({ lat: 51.5072, lng: -0.1276 });
   const [mainState, setMainState] = useState('map');
+  const [voteCount, setVoteCount] = useState(null);
   return (
     <MapDataContext.Provider
       value={[
@@ -21,7 +22,7 @@ export default function MapDataProvider(props) {
         twigletLocationToAdd,
         setTwigletLocationToAdd,
         allTwiglets, 
-        setAllTwiglets, loading, setLoading, gotoTwiglet, setGotoTwiglet, initialLocation, setInitialLocation, mainState, setMainState
+        setAllTwiglets, loading, setLoading, gotoTwiglet, setGotoTwiglet, initialLocation, setInitialLocation, mainState, setMainState, voteCount, setVoteCount
       ]}
     >
       {props.children}
